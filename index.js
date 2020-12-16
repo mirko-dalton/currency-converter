bd427ecb112e743745055fc0c4080c5ad377e14d
Miroslaw PAPIS <info@papis-online.com>
2020-12-16 21:07:02 +0100

    added validations and  bad text fixed

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
 "use strict"
 
 let selectCurrency = document.querySelector('.select-currency-option');
-let currencyInfoSpan = document.querySelector('.currency-info');
+let currencyInfo = document.querySelector('.currency-info');
 let amountConvertAfter = document.querySelector('.amount-convert-after-inp');
 let onButtonClickToConvert = document.getElementById('click-convert-btn');
 let currencySelected = selectCurrency.value;
 
-selectCurrency.addEventListener("change", function () {
+selectCurrency.addEventListener('change', function () {
     currencySelected = this.value;
 });
 
 async function getConvertCurrency() {
 
-    let response = await fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${currencySelected}`)
+    let response = await fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${currencySelected}`);
     let question = await response.json();
-    let result = await question.rates[0].mid
+    let result = await question.rates[0].mid;
     const currencyConverted = result;
-    setConvertCurrency(currencyConverted);
+    calculateConvertCurrency(currencyConverted);
 
 }
 
-function setConvertCurrency(currencyConverted) {
+function calculateConvertCurrency(currencyConverted) {
 
-    const amountConvertBefore = document.querySelector('.amount-convert-before-inp').value;
+    let amountConvertBefore = document.querySelector('.amount-convert-before-inp').value;
+
+    if (amountConvertBefore.length < 1 || amountConvertBefore < 0 || amountConvertBefore == 0) {
+        currencyInfo.innerHTML = 'Podaj sume do przeliczenia!';
+        amountConvertAfter.value = '';
+        return;
+    };
 
     if (currencySelected === 'eur') {
         amountConvertAfter.value = `${(amountConvertBefore * currencyConverted).toFixed(2)} PLN`;
@@ -31,8 +37,8 @@ function setConvertCurrency(currencyConverted) {
     } else if (currencySelected === 'chf') {
         amountConvertAfter.value = `${(amountConvertBefore * currencyConverted).toFixed(2)} PLN`;
     };
+    currencyInfo.innerHTML = 'Aktualny kurs ' + currencySelected.toUpperCase() + ' = ' + `${currencyConverted.toFixed(2)} PLN`;
 
-    currencyInfoSpan.innerHTML = "Aktualny kurs " + currencySelected.toUpperCase() + " = " + `${currencyConverted.toFixed(2)} PLN`;
 };
 
 
