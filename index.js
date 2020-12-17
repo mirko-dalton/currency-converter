e992796b60bf3272f6691a2291faba73e9570c74
Miroslaw PAPIS <info@papis-online.com>
2020-12-17 12:17:41 +0100

    added cath error and currency date

--- index.js
+++ index.js
@@ -6,40 +6,44 @@ let amountConvertAfter = document.querySelector('.amount-convert-after-inp');
 let onButtonClickToConvert = document.getElementById('click-convert-btn');
 let currencySelected = selectCurrency.value;
 
-selectCurrency.addEventListener('change', function () {
+selectCurrency.addEventListener('change', function() {
     currencySelected = this.value;
 });
 
 async function getConvertCurrency() {
 
-    let response = await fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${currencySelected}`);
-    let question = await response.json();
-    let result = await question.rates[0].mid;
-    const currencyConverted = result;
-    calculateConvertCurrency(currencyConverted);
+    fetch('https://api.nbp.pl/api/exchangerates/rates/a/' + currencySelected)
+        .then(resp => resp.json())
+        .then(resp => {
+            resp.rates[0].mid;
+            resp.rates[0].effectiveDate;
+            let result = resp.rates[0].mid;
+            let date = resp.rates[0].effectiveDate;
+            const currencyResult = result;
+            const currencyDate = date;
+            calculateConvertCurrency(currencyResult, currencyDate);
+        })
+        .catch(error => error + alert('Błąd podczas pobierania danych'));
 
 }
 
-function calculateConvertCurrency(currencyConverted) {
+function calculateConvertCurrency(currencyResult, currencyDate) {
 
-    let amountConvertBefore = document.querySelector('.amount-convert-before-inp').value;
+    const amountConvertBefore = document.querySelector('.amount-convert-before-inp').value;
 
     if (amountConvertBefore.length < 1 || amountConvertBefore < 0 || amountConvertBefore == 0) {
-        currencyInfo.innerHTML = 'Podaj sume do przeliczenia!';
+        currencyInfo.innerHTML = 'Podaj sumę do przeliczenia!!';
         amountConvertAfter.value = '';
         return;
     };
 
     if (currencySelected === 'eur') {
-        amountConvertAfter.value = `${(amountConvertBefore * currencyConverted).toFixed(2)} PLN`;
+        amountConvertAfter.value = (amountConvertBefore * currencyResult).toFixed(2) + ' PLN';
     } else if (currencySelected === 'usd') {
-        amountConvertAfter.value = `${(amountConvertBefore * currencyConverted).toFixed(2)} PLN`;
+        amountConvertAfter.value = (amountConvertBefore * currencyResult).toFixed(2) + ' PLN';
     } else if (currencySelected === 'chf') {
-        amountConvertAfter.value = `${(amountConvertBefore * currencyConverted).toFixed(2)} PLN`;
+        amountConvertAfter.value = (amountConvertBefore * currencyResult).toFixed(2) + ' PLN';
     };
-    currencyInfo.innerHTML = 'Aktualny kurs ' + currencySelected.toUpperCase() + ' = ' + `${currencyConverted.toFixed(2)} PLN`;
+    currencyInfo.innerHTML = 'Aktualny kurs ' + currencySelected.toUpperCase() + ' na dzien ' + currencyDate + ' = ' + currencyResult.toFixed(2) + ' PLN';
 
 };
-
-
-
